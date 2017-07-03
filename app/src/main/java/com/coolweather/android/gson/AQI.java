75b12a43b7c7082727189213f91847d934dd39c2
SLJ <m18875764622@163.com>
2017-07-03 15:28:54 +0800

    加入显示天气信息的功能

---
+++ app/src/main/java/com/coolweather/android/gson/AQI.java
@@ -0,0 +1,14 @@
+package com.coolweather.android.gson;
+/**
+ * Created by SLJ on 2017/7/3.
+ */
+
+public class AQI {
+    public AQICity city;
+
+
+    public class AQICity{
+        public String aqi;
+        public String pm25;
+    }
+}
